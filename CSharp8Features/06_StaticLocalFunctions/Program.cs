e5a6535b522248c2a97656ff8ff259d435fa260f
vdimov <vdimov@objectsystems.com>
2020-08-28 12:21:36 +0300

    06_StaticLocalFunctions

---
+++ CSharp8Features/06_StaticLocalFunctions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _06_StaticLocalFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var runner = new Runner();
+            runner.Do();
+        }
+    }
+}
---
+++ CSharp8Features/06_StaticLocalFunctions/Runner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06_StaticLocalFunctions
+{
+    class Runner
+    {
+        public void Do()
+        {
+            int a = 1;
+            int b = 2;
+
+            static int sum(int c, int d) => c + d;
+            Console.WriteLine(sum(1, 2));
+
+            //static int sum2() => a + b; // This will break the code
+        }
+    }
+}
