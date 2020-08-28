f92b8502667a76257d21564a670ec96887d8c6a6
vdimov <vdimov@objectsystems.com>
2020-08-28 12:08:08 +0300

    05_TupleSwitchPattern

---
+++ CSharp8Features/05_TupleSwitchPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05_TupleSwitchPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum = " + GetSum("one", "two"));
+        }
+
+        private static int GetSum(string a, string b)
+        {
+            return (a, b) switch
+            {
+                ("one", "two") => 3,
+                ("two", "three") => 5,
+                _ => throw new ArgumentException();
+            };
+        }
+    }
+}
