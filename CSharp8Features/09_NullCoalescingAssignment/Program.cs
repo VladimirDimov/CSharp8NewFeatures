edaad2195d36e2b6b77cfe4dbb7a163f42e8bbcd
vdimov <vdimov@objectsystems.com>
2020-08-31 14:34:50 +0300

    sync

---
+++ CSharp8Features/09_NullCoalescingAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09_NullCoalescingAssignment
+{
+    class Program
+    {
+        private static ICollection<int> collection;
+
+        static void Main(string[] args)
+        {
+            
+
+            for (int i = 0; i < 10; i++)
+            {
+                collection ??= new List<int>();
+                collection.Add(i);
+            }
+
+            Console.WriteLine(string.Join(", ", collection));
+        }
+    }
+}
